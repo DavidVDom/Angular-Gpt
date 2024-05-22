69efdd98557e8f00f21517de09a2c58ac8e4138c
DavidAsmodeus <david.threshold@gmail.com>
2024-05-22 23:37:07 +0200

    typingLoader component

--- src/app/presentation/pages/orthographyPage/orthographyPage.component.ts
+++ src/app/presentation/pages/orthographyPage/orthographyPage.component.ts
@@ -6,13 +6,28 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 // import { ChatMessageComponent } from '@components/chat-bubbles/chatMessage/chatMessage.component';
 // import { MyMessageComponent } from '@components/chat-bubbles/myMessage/myMessage.component';
 // o también usar directamente el index.ts que hemos creado bajo components
-import { ChatMessageComponent, MyMessageComponent } from '@components/index';
+import {
+  ChatMessageComponent,
+  MyMessageComponent,
+  TextMessageBoxComponent,
+  TypingLoaderComponent,
+} from '@components/index';
 
 @Component({
   selector: 'app-orthography-page',
   standalone: true,
-  imports: [CommonModule, ChatMessageComponent, MyMessageComponent],
+  imports: [
+    CommonModule,
+    ChatMessageComponent,
+    MyMessageComponent,
+    TypingLoaderComponent,
+    TextMessageBoxComponent,
+  ],
   templateUrl: './orthographyPage.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class OrthographyPageComponent {}
+export default class OrthographyPageComponent {
+  handleMessage(prompt: string) {
+    console.log(prompt);
+  }
+}
