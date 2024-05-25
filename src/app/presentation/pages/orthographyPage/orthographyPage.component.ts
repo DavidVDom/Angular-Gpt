d7ed0aff7673a625223d7b57218e1de0a2f65a82
DavidAsmodeus <david.threshold@gmail.com>
2024-05-25 21:08:50 +0200

    modify and add componentes

--- src/app/presentation/pages/orthographyPage/orthographyPage.component.ts
+++ src/app/presentation/pages/orthographyPage/orthographyPage.component.ts
@@ -1,5 +1,10 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  signal,
+} from '@angular/core';
 // import { ChatMessageComponent } from '../../components/chat-bubbles/chatMessage/chatMessage.component';
 // import { MyMessageComponent } from '../../components/chat-bubbles/myMessage/myMessage.component';
 // en tsconfig.json hemos definido en paths alias para la carpeta componentes
@@ -10,8 +15,14 @@ import {
   ChatMessageComponent,
   MyMessageComponent,
   TextMessageBoxComponent,
+  TextMessageBoxEvent,
+  TextMessageBoxFileComponent,
+  TextMessageBoxSelectComponent,
+  TextMessageEvent,
   TypingLoaderComponent,
 } from '@components/index';
+import { Message } from '@interfaces/message.interface';
+import { OpenAiService } from '../../services/openai.service';
 
 @Component({
   selector: 'app-orthography-page',
@@ -22,12 +33,26 @@ import {
     MyMessageComponent,
     TypingLoaderComponent,
     TextMessageBoxComponent,
+    TextMessageBoxFileComponent,
+    TextMessageBoxSelectComponent,
   ],
   templateUrl: './orthographyPage.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export default class OrthographyPageComponent {
+  public messages = signal<Message[]>([{ text: 'Hola Mundo', isGpt: true }]);
+  public isLoading = signal(false);
+  public openAiService = inject(OpenAiService); // es igual que si lo inyectáramos por el constructor
+
   handleMessage(prompt: string) {
     console.log(prompt);
+  }
+
+  handleMessageWithFile({ prompt, file }: TextMessageEvent) {
+    console.log({ prompt, file });
+  }
+
+  handleMessageWithSelect(event: TextMessageBoxEvent) {
+    console.log(event);
   }
 }
---
+++ src/app/presentation/services/openai.service.ts
@@ -0,0 +1,5 @@
+import { Injectable } from '@angular/core';
+
+// se usará este servicio para llamar a casos de uso
+@Injectable({ providedIn: 'root' })
+export class OpenAiService {}
